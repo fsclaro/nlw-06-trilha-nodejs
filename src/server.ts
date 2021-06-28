5a4898243fad435d4dcff6efa34fa61678e54c61
Nando <fsclaro@gmail.com>
2021-06-28 16:17:49 -0300

    feat: adjust user routes and express params

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import 'reflect-metadata'
 import './db'
-
+import { router } from './routes/routes'
 
 const app = express()
 const port = 3000
+
+app.use(express.json())
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Express server is running at port ${port}`)
