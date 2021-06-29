3c96b86fc1609d1ffd32826dc950acc71cf2f711
Nando <fsclaro@gmail.com>
2021-06-28 21:29:22 -0300

    feat: create Tag class

---
+++ src/entities/Tag.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("tags")
+class Tag {
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export { Tag };
