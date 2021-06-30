5cd41da4f431b216493063c1673bdfd938c5dde9
Nando <fsclaro@gmail.com>
2021-06-30 14:42:39 -0300

    refactor: tags entitie

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -6,6 +6,7 @@ import {
   UpdateDateColumn,
 } from "typeorm";
 import { v4 as uuid } from "uuid";
+import { Expose } from "class-transformer";
 
 @Entity("tags")
 class Tag {
@@ -26,6 +27,11 @@ class Tag {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'name_custom' })
+  name_custom(): string {
+    return `#${this.name.replace(" ","").toLowerCase()}`
+  }
 }
 
 export { Tag };
